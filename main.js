5579ad2d4be526afe82c223faad78a7adb6ab07a
JoeFu <fuzeqi@gmail.com>
2019-04-02 14:08:04 +1030

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// date format
+$.validator.addMethod(
+    "australianDate",
+    function(value, element) {
+        // put your own logic here, this is just a (crappy) example
+        return value.match(/^\d\d?\/\d\d?\/\d\d\d\d$/);
+    },
+    "Please enter a date in the format dd/mm/yyyy."
+);
+/*

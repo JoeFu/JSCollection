3c66a632a7da030b568439dd5165bcd71c520c36
JoeFu <fuzeqi@gmail.com>
2019-04-20 20:31:31 +0930

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,34 @@ $.validator.addMethod(
     "Please enter a date in the format dd/mm/yyyy."
 );
 /*
+
+
+<script>
+//移动设备判断
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i) ? true : false;
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) ? true : false;
+    },
+Phone: function() {
+return navigator.userAgent.match(/Mobile/i) ? true : false;
+},
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.Windows() || isMobile.Phone());
+    }
+};
+
+function isIE () {
+var myNav = navigator.userAgent.toLowerCase();
+return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+};
+
+</script>
